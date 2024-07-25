f53aa4f85253bc732bb0df3a16e7b7c820c020cb
gmahur7 <gm7017308@gmail.com>
2024-07-25 08:25:01 +0530

    first

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app= express();
+
+const http = require('http');
+const path = require('path');
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io=socketio(server);
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")))
+
+io.on("connection",(socket)=>{
+    socket.on("send-location",(data)=>{
+        io.emit("recieve-location",{
+            id:socket.id,
+            ...data
+        })
+    })
+    console.log("connected")
+
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected",{id:socket.id})
+    })
+})
+
+app.get("/",(req,resp)=>{
+    resp.render("index")
+})
+
+server.listen(8000,()=>{
+    console.log("Server is running @ 8000")
+})
